1158d648bf338146659749b9b2f76f2d065331be
Grace <141918570+gbuttler@users.noreply.github.com>
2024-01-28 13:30:26 +0000

    updates the basic js code and adds the new HTML file

---
+++ foundations-calculator.js
@@ -0,0 +1,24 @@
+let firstNumber = 1;
+
+let secondNumber = 2;
+
+
+
+
+
+
+
+let add = (a,b) => a+b;
+
+let subtract = (a,b) => a-b;
+
+let multiply = (a,b) => a*b;
+
+let divide = (a,b) => a/b;
+
+let operator = add; 
+
+
+
+let operate = (a,b,c) => c(a,b);
+
