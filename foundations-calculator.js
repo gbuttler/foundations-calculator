8686401ae21f74a0797c07c6e4181100b52d6b29
Grace <141918570+gbuttler@users.noreply.github.com>
2024-02-14 11:20:50 +0000

    add some code to make all of the buttons display something. Also began to work on functions to make the numbers interact.

--- foundations-calculator.js
+++ foundations-calculator.js
@@ -1,12 +1,21 @@
-let firstNumber = 1;
+let firstNumber = document.getElementById('a');
 
-let secondNumber = 2;
+let secondNumber = document.getElementById('b');
 
-
-
-
+let operator = document.getElementById('operator')
 
 
+const result = () => {
+    if (('span').attr('operator') == '+') 
+        document.getElementById('result').innerHTML= (firstNumber,secondNumber) => firstNumber+secondNumber;
+};
+
+{/* <div id="myID"></div>
+
+if($("div").attr("id") == "myID")
+{
+    //do stuff
+} */}
 
 let add = (a,b) => a+b;
 
@@ -16,13 +25,66 @@ let multiply = (a,b) => a*b;
 
 let divide = (a,b) => a/b;
 
-let operator = add; 
+// let operator = add; 
 
 
 
 let operate = (a,b,c) => c(a,b);
 
 
-const test = () => {
+const testOne = () => {
     document.getElementById('display').innerHTML += '1'; //use += to stop numbers overwriting themselves (I think)
 };
+
+const testTwo = () => {
+    document.getElementById('display').innerHTML += '2'; //use += to stop numbers overwriting themselves (I think)
+}
+
+const testThree = () => {
+    document.getElementById('display').innerHTML += '3'; //use += to stop numbers overwriting themselves (I think)
+}
+
+const testFour = () => {
+    document.getElementById('display').innerHTML += '4'; //use += to stop numbers overwriting themselves (I think)
+}
+
+const testFive = () => {
+    document.getElementById('display').innerHTML += '5'; //use += to stop numbers overwriting themselves (I think)
+}
+
+const testSix = () => {
+    document.getElementById('display').innerHTML += '6'; //use += to stop numbers overwriting themselves (I think)
+}
+
+const testSeven = () => {
+    document.getElementById('display').innerHTML += '7'; //use += to stop numbers overwriting themselves (I think)
+}
+
+const testEight = () => {
+    document.getElementById('display').innerHTML += '8'; //use += to stop numbers overwriting themselves (I think)
+}
+
+const testNine = () => {
+    document.getElementById('display').innerHTML += '9'; //use += to stop numbers overwriting themselves (I think)
+}
+
+const testZero = () => {
+    document.getElementById('display').innerHTML += '0'; //use += to stop numbers overwriting themselves (I think)
+}
+
+const testAdd = () => {
+    document.getElementById('display').innerHTML += '+'; //use += to stop numbers overwriting themselves (I think)
+}
+
+const testSubtract = () => {
+    document.getElementById('display').innerHTML += '-'; //use += to stop numbers overwriting themselves (I think)
+}
+
+const testMultiply = () => {
+    document.getElementById('display').innerHTML += '*'; //use += to stop numbers overwriting themselves (I think)
+}
+
+const testDivide = () => {
+    document.getElementById('display').innerHTML += '/'; //use += to stop numbers overwriting themselves (I think)
+}
+
